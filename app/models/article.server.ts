f687dca94514c6136e32b3836fce76c835319280
kleiba <k.leiba@free.fr>
2022-07-19 01:34:34 +0200

    Feature/prisma unit test (#2)
    * protected more .env files
    
    * updated seed
    
    * added setup for unit tests with dotenv
    
    * Added config to get db test to work synchronously
    
    * updated article tests to test config
    
    * fixed missing function launch

--- app/models/article.server.ts
+++ app/models/article.server.ts
@@ -2,7 +2,10 @@ import type { Article } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
-
 export function getArticles() {
   return prisma.article.findMany()
+}
+
+export function createArticle({ content, image, introduction, title }: { content: Article['content'], image: Article['image'], introduction: Article['introduction'], title: Article['title'] }) {
+  return prisma.article.create({ data: { content, image, introduction, title } })
 }
