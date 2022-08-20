fcc86e81ed8868b0d29f43c3b8a4c86f94676a5c
kevinleiba <k.leiba@free.fr>
2022-08-20 23:13:43 +0200

    Refactoring on Image model

--- app/models/image.server.test.ts
+++ app/models/image.server.test.ts
@@ -1,12 +1,9 @@
 import { unSeed } from '../../prisma/unSeed'
 import { createImage } from './image.server'
 import { url, description } from '../../mocks/image'
-// import * as articleMock from '../../mocks/article'
-import {/* Article ,*/ Image, PrismaClient } from "@prisma/client";
+import { Image, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-let image: Image | null = null
-// let article: Article | null = null
 
 describe("Image model", () => {
   beforeAll(async () => {
@@ -24,15 +21,5 @@ describe("Image model", () => {
     expect(images).toHaveLength(1)
     expect(images[0].description).toBe(description)
     expect(images[0].url).toBe(url)
-
-    image = images[0]
   })
-
-  // test("can link an image to an article", async () => {
-  //   article = await prisma.article.create({ data: { published: articleMock.published } })
-
-  //   await linkImageToArticle({ imageUuid: image!.uuid, articleUuid: article.uuid })
-  //   const updatedArticle = await prisma.article.findUnique({ where: { uuid: article.uuid }, include: { image: true } })
-  //   expect(updatedArticle?.image?.uuid).toBe(image?.uuid)
-  // })
 })
--- app/models/image.server.ts
+++ app/models/image.server.ts
@@ -1,21 +1,7 @@
-import type { /*Article,*/ Image } from "@prisma/client";
+import type { Image } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
 export function createImage({ url, description }: { url: Image['url'], description: Image['description'] }) {
   return prisma.image.create({ data: { url, description } })
 }
-
-// export async function linkImageToArticle({ imageUuid, articleUuid }: { imageUuid: Image['uuid'], articleUuid: Article['uuid'] }) {
-//   await prisma.article.update(({
-//     where: { uuid: articleUuid },
-//     data: { image: { connect: { uuid: imageUuid } } }
-//   }))
-// }
-
-// export async function linkImageToSection({ imageUuid, sectionUuid }: { imageUuid: Image['uuid'], sectionUuid: Article['uuid'] }) {
-//   await prisma.section.update(({
-//     where: { uuid: sectionUuid },
-//     data: { images: { connect: [{ uuid: imageUuid }] } }
-//   }))
-// }
