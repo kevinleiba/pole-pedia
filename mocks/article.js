4871140731fd6e921ebc192d95c67da37aa960ad
kleiba <k.leiba@free.fr>
2022-08-23 03:13:19 +0200

    Feature/display article (#5)
    * Updated seed because no more image to article
    
    * Added missing method to article model
    
    * Updated homepage to show article and link to detail
    
    * Added db object to mock. But not ready yet

--- mocks/article.js
+++ mocks/article.js
@@ -1,5 +1,13 @@
 const published = true
 
+const dbObject = {
+  "uuid": "b682c8fe-429d-4917-999b-f308516e3f12",
+  "published": published,
+  "createdAt": "2022-08-20T22:06:51.915Z",
+  "updatedAt": "2022-08-20T22:06:51.916Z",
+}
+
 module.exports = {
-  published
+  published,
+  dbObject
 }
--- mocks/section.js
+++ mocks/section.js
@@ -9,8 +9,21 @@ yet another section....
 const order = 0
 const title = "Batman"
 
+
+
+const dbObject = {
+  "uuid": "81957122-1b02-4533-b094-08f5bccfb24f",
+  "content": content,
+  "order": order,
+  "title": title,
+  "articleUuid": "b682c8fe-429d-4917-999b-f308516e3f12",
+  "createdAt": "2022-08-20T22:06:51.920Z",
+  "updatedAt": "2022-08-20T22:06:51.921Z"
+}
+
 module.exports = {
   content,
+  dbObject,
   order,
   title
 }
