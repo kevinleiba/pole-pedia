8998cce119d514ee630f8af1fd4636fe97934b10
kevinleiba <k.leiba@free.fr>
2022-08-25 21:23:32 +0200

    updated cypress commands for db

---
+++ prisma/seeder.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+const articleMock = require('../mocks/article')
+const imageMock = require('../mocks/image')
+const informationMock = require('../mocks/information')
+const sectionMock = require('../mocks/section')
+const prisma = new PrismaClient();
+
+export async function seed() {
+
+  await prisma.section.deleteMany()
+  await prisma.information.deleteMany()
+  await prisma.image.deleteMany()
+  await prisma.article.deleteMany()
+
+
+  const introImage = await prisma.image.create({ data: { description: imageMock.description, url: imageMock.url } })
+  const article = await prisma.article.create({ data: { published: articleMock.published } })
+
+  const introSection = await prisma.section.create({ data: { content: sectionMock.content, order: sectionMock.order, title: sectionMock.title, articleUuid: article.uuid, images: { connect: [{ uuid: introImage.uuid }] } } })
+  const info = await prisma.information.create({ data: { description: informationMock.description, title: informationMock.title, articleUuid: article.uuid } })
+
+  const firstSubSection = await prisma.section.create({ data: { title: "FirstSubSection", content: "sub section content", order: 0 } })
+  const firstSection = await prisma.section.create({ data: { title: "FirstSection", order: 1, subSections: { connect: [{ uuid: firstSubSection.uuid }] }, articleUuid: article.uuid } })
+
+
+  console.log(`Database has been seeded. 🌱`);
+}
