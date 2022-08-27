ba8e6bf40bd09a2307b66cd36da3a95792087f48
kevinleiba <k.leiba@free.fr>
2022-08-28 01:05:59 +0200

    updated obsolete test

--- app/components/Section.test.tsx
+++ app/components/Section.test.tsx
@@ -5,12 +5,11 @@ import Section from './Section'
 describe("Section", () => {
   const content = "Batman is a superhero appearing in American comic books published by DC Comics."
   test("can render content", () => {
-    const onUpdate = vi.fn()
-    render(<Section content={`<p>${content}</p>`} onUpdate={onUpdate} />)
+    render(<Section content={`<p>${content}</p>`} onBlur={() => { }} />)
 
 
     const firstLine = document.querySelector('p')
     expect(firstLine?.innerText).toBe(content)
-    // can't figure how to test onUpdate...
+    // TODO --> test on blur
   })
 })
