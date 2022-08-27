aa92485900afd3221cb1fd63977282fb9a00613b
kevinleiba <k.leiba@free.fr>
2022-08-27 21:51:52 +0200

    Added unit tests for Section

--- app/components/Section.test.tsx
+++ app/components/Section.test.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
 import { render } from '@testing-library/react'
 
 import Section from './Section'
 
 describe("Section", () => {
-  test("can render without crashing", () => {
-    render(<Section />)
+  const content = "Batman is a superhero appearing in American comic books published by DC Comics."
+  test("can render content", () => {
+    const onUpdate = vi.fn()
+    render(<Section content={`<p>${content}</p>`} onUpdate={onUpdate} />)
 
+
+    const firstLine = document.querySelector('p')
+    expect(firstLine?.innerText).toBe(content)
+    // can't figure how to test onUpdate...
   })
 })
