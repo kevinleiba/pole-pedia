f2c328952d38e444c03742dbb948be43b919b66d
kevinleiba <k.leiba@free.fr>
2022-08-28 20:06:15 +0200

    Can now update information

--- app/models/information.server.ts
+++ app/models/information.server.ts
@@ -8,3 +8,7 @@ export function createInformation({ title, description, articleUuid }: { title: 
 export function deleteInformation({ uuid }: { uuid: Information['uuid'] }) {
   return prisma.information.delete({ where: { uuid } })
 }
+
+export function updateInformation({ uuid, title, description }: { uuid: Information['uuid'], title: Information['title'], description: Information['description'] }) {
+  return prisma.information.update({ where: { uuid }, data: { title, description } })
+}
