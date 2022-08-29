8fa0c25cd064546f1818f442c3ddc3304f214ea9
kevinleiba <k.leiba@free.fr>
2022-08-30 01:18:33 +0200

    Homepage now allows research

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,18 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { getAllArticles } from "~/models/article.server";
+import { json, LoaderFunction } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { getAllArticles, searchArticles } from "~/models/article.server";
+import logo from "~/components/icons/logo.svg";
 
 type LoaderData = {
-  articles: Awaited<ReturnType<typeof getAllArticles>>;
+  articles: Awaited<ReturnType<typeof getAllArticles | typeof searchArticles>>;
 };
 
-export const loader = async () => {
-  return json<LoaderData>({
-    articles: await getAllArticles(),
-  });
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const search = url.searchParams.get("search");
+
+  if (!search) return json<LoaderData>({ articles: await getAllArticles() })
+  return json<LoaderData>({ articles: await searchArticles({ search }) })
 };
 
 
@@ -17,12 +20,23 @@ export default function Index() {
   const { articles } = useLoaderData() as LoaderData;
 
   return (
-    <div>
-      {[articles.map(article => (
-        <div key={article.uuid}>
-          <Link to={`/article/${article.uuid}`} ><h1>{article.sections[0].title}</h1></Link>
-        </div>)
-      )]}
+    <div className="p-xl">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="mb-xs">Pole Pedia</h1>
+        <p className="text-xs mb-xl">Your Encyclopieda</p>
+        <img src={logo} className="h-[300px] w-[300px] object-fit" alt="pole pedia logo" />
+        <Form method="get" action="/" className="max-w-[717px] w-full mt-xl">
+          <input className="border border-darkGrey w-full text-xl px-xs article-search" name="search" type="text" placeholder="Search article..." />
+        </Form>
+        <div className="mt-xl p-m w-full max-w-[1200px] border border-black">
+          <h2 className="mb-m">Latest articles:</h2>
+          {articles.map(article => (
+            <div key={article.uuid} className="mb-s">
+              <Link to={`/article/${article.uuid}`}><h3 className="hover:underline">{article.sections[0].title}</h3></Link>
+            </div>)
+          )}
+        </div>
+      </div>
     </div>
   )
 }
