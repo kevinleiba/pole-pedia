9b044ebdde0f5cadba96bb7c38b2901eb0f18f5d
kevinleiba <k.leiba@free.fr>
2022-08-29 15:05:20 +0200

    updated menu content

--- app/routes/article.tsx
+++ app/routes/article.tsx
@@ -11,7 +11,16 @@ export default function ArticlePage() {
       )
       }>
         <div className="h-[100px] w-[100px]" >
-          <Link to="/"><img src={logo} alt="polepedia logo" /></Link>
+          <Link to="/">
+            <img src={logo} alt="polepedia logo" />
+            <h3 className="text-center mt-xs font-light">Pole Pedia</h3>
+            <p className="text-xs text-center">Your Encyclopieda</p>
+          </Link>
+          <div className="mt-l flex flex-col">
+            <Link to="/" className="text-darkBlue hover:underline text-xs mb-xs">Main page</Link>
+            <Link to="/" className="text-darkBlue hover:underline text-xs mb-xs">Random Article</Link>
+            <Link to="/article/new" className="text-darkBlue hover:underline text-xs">Create Article</Link>
+          </div>
         </div>
         <div className="absolute w-[1px] z-10 top-0 bottom-0 right-0 bg-gradient-to-b from-white via-vividBlue to-vividBlue"></div>
       </div>
